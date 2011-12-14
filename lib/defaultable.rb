916130fa44128c2582bd71f496ec085da717ee0b
Robert Ross <robert@maintainedauto.com>
2011-12-13 20:14:13 -0800

    Spaces for indentation, damn you sublime text

--- lib/defaultable.rb
+++ lib/defaultable.rb
@@ -3,85 +3,85 @@ require 'yaml'
 require 'active_support/core_ext/class/attribute'
 
 module Defaultable
-	class Settings
-		class_attribute :defaults
-		attr_accessor :root_key, :parent
+  class Settings
+    class_attribute :defaults
+    attr_accessor :root_key, :parent
 
-	  def initialize(hash=nil, root_key=nil, parent=nil)
-	    @table = {}
+    def initialize(hash=nil, root_key=nil, parent=nil)
+      @table = {}
 
-	    if root_key
-	    	self.root_key = root_key
-	    end
+      if root_key
+        self.root_key = root_key
+      end
 
-	    if parent
-	    	self.parent = parent
-	    end
-	    
-	    if !hash.nil? && hash.kind_of?(Hash)
-		    hash.each do |key, value|
-		      if value.kind_of?(Hash)
-		        send("#{key}=", Defaultable::Settings.new(value, key, self))
-		      else
-		        self.send("#{key}=", value)
-		      end
-		    end
-		  end
-	  end
-	  
-	  def method_missing(message, *args, &block)
-	  	# puts "Message: #{message}", "Args: #{args}", "\n"
+      if parent
+        self.parent = parent
+      end
+      
+      if !hash.nil? && hash.kind_of?(Hash)
+        hash.each do |key, value|
+          if value.kind_of?(Hash)
+            send("#{key}=", Defaultable::Settings.new(value, key, self))
+          else
+            self.send("#{key}=", value)
+          end
+        end
+      end
+    end
+    
+    def method_missing(message, *args, &block)
+      # puts "Message: #{message}", "Args: #{args}", "\n"
 
-	  	if !(message.to_s =~ /^(.*)=$/) && @table.has_key?(message.to_s)
-	  		@table[message.to_s]
-	  	elsif message.to_s =~ /^(.*)=$/
-	  		@table[message.to_s.gsub(/=$/, '')] = args.first
-	  	elsif !(message.to_s =~ /^(.*)=$/)
-	  		# We're looking for a default
-	  		default_settings = self.class.defaults
+      if !(message.to_s =~ /^(.*)=$/) && @table.has_key?(message.to_s)
+        @table[message.to_s]
+      elsif message.to_s =~ /^(.*)=$/
+        @table[message.to_s.gsub(/=$/, '')] = args.first
+      elsif !(message.to_s =~ /^(.*)=$/)
+        # We're looking for a default
+        default_settings = self.class.defaults
 
-	  		# If we have a parent, we need to traverse to the top of all of the method calls (root_keys), then go back down them
-	  		if self.parent
-	  			root_keys = []
-	  			current_parent = self
-	  			while current_parent.has_parent? do
-	  				root_keys << current_parent.root_key
-	  				current_parent = current_parent.parent
-	  			end
+        # If we have a parent, we need to traverse to the top of all of the method calls (root_keys), then go back down them
+        if self.parent
+          root_keys = []
+          current_parent = self
+          while current_parent.has_parent? do
+            root_keys << current_parent.root_key
+            current_parent = current_parent.parent
+          end
 
-	  			root_keys.reverse.each do |key|
-	  				default_settings = default_settings.send(key)
-	  			end
+          root_keys.reverse.each do |key|
+            default_settings = default_settings.send(key)
+          end
 
-	  			return default_settings.send(message)
-	  		# If don't have a parent, just send the message to the defaults outright 
-	  		else
-	  			default_settings.send(message)
-	  		end
-	  	end
-	  end
-	  
-	  def as_hash
-	    @table
-	  end
+          return default_settings.send(message)
+        # If don't have a parent, just send the message to the defaults outright 
+        else
+          default_settings.send(message)
+        end
+      end
+    end
+    
+    def as_hash
+      @table
+    end
 
-	  def has_parent?
-	  	!!self.parent
-	  end
-	  
-	  class << self
-	    def set_defaults(settings, env=nil)
-	    	case settings
-	    	when Hash
-	    		self.defaults = Defaultable::Settings.new(settings)
-	    	when String
-	    		yaml = YAML.load_file(settings)
-	    		yaml = yaml[env] if env
-	    		self.defaults = Defaultable::Settings.new(yaml)
-	    	else
-	    		raise ArgumentError, "You must supply a hash or a file name for default settings"
-	    	end
-	    end
-	  end
-	end
+    def has_parent?
+      !!self.parent
+    end
+    
+    class << self
+      def set_defaults(settings, env=nil)
+        case settings
+        when Hash
+          self.defaults = Defaultable::Settings.new(settings)
+        when String
+          yaml = YAML.load_file(settings)
+          yaml = yaml[env] if env
+          self.defaults = Defaultable::Settings.new(yaml)
+        else
+          raise ArgumentError, "You must supply a hash or a file name for default settings"
+        end
+      end
+    end
+  end
 end
--- spec/defaultable_spec.rb
+++ spec/defaultable_spec.rb
@@ -1,124 +1,124 @@
 require 'spec_helper'
 
 describe Defaultable::Settings do
-	before :each do
-		Defaultable::Settings.set_defaults({})
-	end
+  before :each do
+    Defaultable::Settings.set_defaults({})
+  end
 
-	it "should not have a parent" do
-		setting = Defaultable::Settings.new
-		setting.has_parent?.should be_false
-	end
+  it "should not have a parent" do
+    setting = Defaultable::Settings.new
+    setting.has_parent?.should be_false
+  end
 
-	it "should initialize with a hash" do
-		setting = Defaultable::Settings.new({:child => 'Bobert'})
-		setting.child.should eq('Bobert')
-	end
+  it "should initialize with a hash" do
+    setting = Defaultable::Settings.new({:child => 'Bobert'})
+    setting.child.should eq('Bobert')
+  end
 
-	it "should be able to set nonexsistent keys" do
-		setting = Defaultable::Settings.new
-		setting.child = 'Bri Bri'
-		setting.child.should eq('Bri Bri')
-	end
+  it "should be able to set nonexsistent keys" do
+    setting = Defaultable::Settings.new
+    setting.child = 'Bri Bri'
+    setting.child.should eq('Bri Bri')
+  end
 
-	it "should be able to set nonexsistent keys to another setting" do
-		setting = Defaultable::Settings.new
-		setting.child = Defaultable::Settings.new
-		setting.child.name = 'Mocha'
-		setting.child.name.should eq('Mocha')
-	end
+  it "should be able to set nonexsistent keys to another setting" do
+    setting = Defaultable::Settings.new
+    setting.child = Defaultable::Settings.new
+    setting.child.name = 'Mocha'
+    setting.child.name.should eq('Mocha')
+  end
 
-	it "should accept nested hashes" do
-		setting = Defaultable::Settings.new({:parent => {:child => 'Rob'}})
-		setting.parent.child.should eq('Rob')
-	end
+  it "should accept nested hashes" do
+    setting = Defaultable::Settings.new({:parent => {:child => 'Rob'}})
+    setting.parent.child.should eq('Rob')
+  end
 
-	it "should have default settings" do
-		Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
-		Defaultable::Settings.defaults.should be_kind_of(Defaultable::Settings)
-	end
+  it "should have default settings" do
+    Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
+    Defaultable::Settings.defaults.should be_kind_of(Defaultable::Settings)
+  end
 
-	it "should have a default setting for a key" do
-		Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
+  it "should have a default setting for a key" do
+    Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
 
-		setting = Defaultable::Settings.new
-		setting.grandparent.should be_kind_of Defaultable::Settings
-	end
+    setting = Defaultable::Settings.new
+    setting.grandparent.should be_kind_of Defaultable::Settings
+  end
 
-	it "should have another key for another setting" do
-		Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
+  it "should have another key for another setting" do
+    Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
 
-		setting = Defaultable::Settings.new
-		setting.grandparent.child.grandchild1.should eq('robert')
-	end
+    setting = Defaultable::Settings.new
+    setting.grandparent.child.grandchild1.should eq('robert')
+  end
 
-	it "should set a key but still have defaults" do
-		Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
+  it "should set a key but still have defaults" do
+    Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
 
-		setting = Defaultable::Settings.new({
-			:grandparent => {
-				:child => {
-					:grandchild3 => 'hurdur'
-				}
-			}
-		})
+    setting = Defaultable::Settings.new({
+      :grandparent => {
+        :child => {
+          :grandchild3 => 'hurdur'
+        }
+      }
+    })
 
-		setting.grandparent.child.grandchild3.should eq('hurdur')
-		setting.grandparent.child.grandchild2.should eq('brian')
-	end
+    setting.grandparent.child.grandchild3.should eq('hurdur')
+    setting.grandparent.child.grandchild2.should eq('brian')
+  end
 
-	it "should be able to overwrite a default" do
-		Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
+  it "should be able to overwrite a default" do
+    Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
 
-		setting = Defaultable::Settings.new({
-			:grandparent => {
-				:child => {
-					:grandchild3 => 'hurdur'
-				}
-			}
-		})
+    setting = Defaultable::Settings.new({
+      :grandparent => {
+        :child => {
+          :grandchild3 => 'hurdur'
+        }
+      }
+    })
 
-		setting.grandparent.child.grandchild3.should eq('hurdur')
-		setting.grandparent.child.grandchild2 = 'drpepper'
-		setting.grandparent.child.grandchild2.should eq('drpepper')
-	end
+    setting.grandparent.child.grandchild3.should eq('hurdur')
+    setting.grandparent.child.grandchild2 = 'drpepper'
+    setting.grandparent.child.grandchild2.should eq('drpepper')
+  end
 
-	it "should be able to set a key in the middle of defaults" do
-		Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
+  it "should be able to set a key in the middle of defaults" do
+    Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/test.yml'
 
-		setting = Defaultable::Settings.new({
-			:grandparent => {
-				:child => {
-					:grandchild3 => 'hurdur'
-				}
-			}
-		})
+    setting = Defaultable::Settings.new({
+      :grandparent => {
+        :child => {
+          :grandchild3 => 'hurdur'
+        }
+      }
+    })
 
-		setting.grandparent.child.grandchild3.should eq('hurdur')
-		setting.grandparent.someotherkey = 'saweet'
-		setting.grandparent.someotherkey.should eq('saweet')
-	end
+    setting.grandparent.child.grandchild3.should eq('hurdur')
+    setting.grandparent.someotherkey = 'saweet'
+    setting.grandparent.someotherkey.should eq('saweet')
+  end
 
-	it "should be able to set a hash for defaults" do
-		Defaultable::Settings.set_defaults :child => 'sxephil'
+  it "should be able to set a hash for defaults" do
+    Defaultable::Settings.set_defaults :child => 'sxephil'
 
-		setting = Defaultable::Settings.new
-		setting.child.should eq('sxephil')
-	end
+    setting = Defaultable::Settings.new
+    setting.child.should eq('sxephil')
+  end
 
-	it "should accept a filename with an environment" do
-		Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/env_test.yml', 'development'
+  it "should accept a filename with an environment" do
+    Defaultable::Settings.set_defaults File.expand_path('../', __FILE__) + '/env_test.yml', 'development'
 
-		setting = Defaultable::Settings.new
-		setting.setting_key.should eq('somevalue')
-	end
+    setting = Defaultable::Settings.new
+    setting.setting_key.should eq('somevalue')
+  end
 
-	it "should be extendable" do
-		class DummySetting < Defaultable::Settings
-			set_defaults :movie => 'Iron Man'
-		end
+  it "should be extendable" do
+    class DummySetting < Defaultable::Settings
+      set_defaults :movie => 'Iron Man'
+    end
 
-		setting = DummySetting.new
-		setting.movie.should eq('Iron Man')
-	end
+    setting = DummySetting.new
+    setting.movie.should eq('Iron Man')
+  end
 end
