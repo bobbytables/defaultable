a9ac491034468817572d4ee850cab3748766d9d7
Robert Ross <robert@maintainedauto.com>
2011-12-22 09:57:58 -0800

    Add serialization specs.

--- spec/serialization_spec.rb
+++ spec/serialization_spec.rb
@@ -3,10 +3,65 @@ require 'spec_helper'
 describe Defaultable::Serialization do
 	before(:each) do
 		class DummySetting < Defaultable::Settings; end
+		Defaultable::Serialization.settings_class = DummySetting
 	end
 
 	it "should set a class for settings" do
-		Defaultable::Serialization.settings_class = DummySetting
 		Defaultable::Serialization.settings_class.should eq(DummySetting)
+	end
+
+	it "should serialize a settings object" do
+		encoder  = Defaultable::Serialization.new
+		settings = DummySetting.new
+
+		settings.foo = 'bar'
+		serialized = encoder.dump(settings)
+		serialized.should be_a String
+	end
+
+	it "should deserialize a settings object back to a kind of Defaultable::Settings" do
+		encoder  = Defaultable::Serialization.new
+		settings = DummySetting.new
+
+		settings.foo = 'bar'
+		serialized = encoder.dump(settings)
+
+		encoder.load(serialized).should be_kind_of Defaultable::Settings
+	end
+
+	it "should throw an exception when the class doesn't match the settings class on load" do
+		encoder = Defaultable::Serialization.new
+
+		lambda { encoder.load('adfghjsfjdhg') }.should raise_error TypeError
+	end
+
+	it "should throw an exception when the class doesn't match the settings class on dump" do
+		encoder = Defaultable::Serialization.new
+
+		lambda { encoder.dump('adfghjsfjdhg'.to_yaml) }.should raise_error TypeError
+	end
+
+	it "should load with keys still in tact" do
+		encoder  = Defaultable::Serialization.new
+		settings = DummySetting.new
+
+		settings.foo = 'bar'
+		serialized   = encoder.dump(settings)
+		unserialized = encoder.load(serialized)
+
+		unserialized.foo.should eq('bar')
+	end
+
+	it "should load defaults in after the fact" do
+		encoder  = Defaultable::Serialization.new
+		settings = DummySetting.new
+
+		settings.foo = 'bar'
+		serialized = encoder.dump(settings)
+
+		DummySetting.set_defaults :bobby => 'tables'
+
+		unserialized = encoder.load(serialized)
+		unserialized.bobby?.should be_true
 	end
 end
