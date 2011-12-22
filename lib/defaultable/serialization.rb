e0909c21d162153c8b178eebb090634ce1c6ce19
Robert Ross <robert@maintainedauto.com>
2011-12-22 09:57:18 -0800

    YAML load instead of parse. Add a comment to describe why we re-instantiate objects.

--- lib/defaultable/serialization.rb
+++ lib/defaultable/serialization.rb
@@ -5,9 +5,13 @@ module Defaultable
 	  # Called to deserialize data to ruby object.
 	  def load(data)
 	  	if data
-	    	obj = YAML.parse(data)
+	    	obj = YAML.load(data)
 
-	    	raise TypeError, "Deserialized object is not of type #{self.class.settings_class.name}." unless obj.is_a?(self.class.settings_class)
+	    	raise TypeError, "Deserialized object is not of type #{self.class.settings_class.name}. Got #{obj.class}" unless obj.is_a?(self.class.settings_class)
+
+	    	# The reason we do the following is because defaults may have changed
+	    	# after the point of serialization. To avoid not having them, we instantiate the class
+	    	# again, we the previous values. This prevents not having data.
 	    	obj.class.new(obj.as_hash)
 	    end
 	  end
@@ -15,7 +19,7 @@ module Defaultable
 	  # Called to convert from ruby object to serialized data.
 	  def dump(obj)
 	  	raise TypeError, "Serialization failed: Object is not of type #{self.class.settings_class.name}." unless obj.is_a?(self.class.settings_class)
-	    YAML.load(obj) if obj
+	    obj.to_yaml if obj
 	  end
 	end
 end
