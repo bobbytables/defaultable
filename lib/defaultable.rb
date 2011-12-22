6da181c4ac3b3f09735e48b9d92e77b8bda3cc7a
Robert Ross <robert@maintainedauto.com>
2011-12-22 13:03:13 -0800

    Autoload registry.

--- lib/defaultable.rb
+++ lib/defaultable.rb
@@ -5,4 +5,5 @@ require 'active_support/core_ext/class/attribute'
 module Defaultable
   autoload :Serialization, 'defaultable/serialization'
   autoload :Settings, 'defaultable/settings'
+  autoload :Registry, 'defaultable/registry'
 end
