f27865645f3dc1441c73377bbad990d99529f7ca
Robert Ross <robert@maintainedauto.com>
2011-12-22 16:54:33 -0800

    Fix grammar.

--- spec/registry_spec.rb
+++ spec/registry_spec.rb
@@ -10,32 +10,32 @@ describe Defaultable::Registry do
 		DummySetting.set_defaults Hash.new
 	end
 
-	it "should log settings to the registry" do
+	it "should log settings to the registry." do
 		setting = DummySetting.new
 		setting.blah = 'asdf'
 
 		setting.registry.as_hash.should have_key('blah')
 	end
 
-	it "should not log defaults to the registry" do
+	it "should not log defaults to the registry." do
 		DummySetting.set_defaults :foo => 'bar'
 
 		setting = DummySetting.new
 		setting.registry.as_hash.should_not have_key('foo')
 	end
 
-	it "should set registry values for nested attributes" do
+	it "should set registry values for nested attributes." do
 		setting = DummySetting.new(:stuff => {:bar => 'foo'})
 		setting.registry.as_hash['stuff'].should have_key('bar')
 	end
 
-	it "should not set nested attributes registry values even in nested defaults" do
+	it "should not set nested attributes registry values even in nested defaults." do
 		DummySetting.set_defaults :values => {:foo => 'bar'}
 
 		DummySetting.new.registry.as_hash.should_not have_key('values')
 	end
 
-	it "should set nested attributes registry values even in nested defaults" do
+	it "should set nested attributes registry values even in nested defaults." do
 		DummySetting.set_defaults :values => {:foo => 'bar'}
 
 		setting = DummySetting.new(:values => {:bar => 'foo'})
@@ -43,7 +43,7 @@ describe Defaultable::Registry do
 		setting.values.bar?.should be_true
 	end
 
-	it "should set nested attributes registry values and retain nested defaults" do
+	it "should set nested attributes registry values and retain nested defaults." do
 		DummySetting.set_defaults :values => {:foo => 'bar'}
 
 		setting = DummySetting.new(:values => {:bar => 'foo'})
