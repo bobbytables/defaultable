a57b14712845cb0a8cc13dc3b81a0a25eb926374
Robert Ross <robert@maintainedauto.com>
2012-03-06 21:53:45 -0800

    On load, using the settings class regardless to create an instance.

--- lib/defaultable/serialization.rb
+++ lib/defaultable/serialization.rb
@@ -1,26 +1,24 @@
 module Defaultable
 	class Serialization
-		class_attribute :settings_class
+		cattr_accessor :settings_class
 
 	  # Called to deserialize data to ruby object.
 	  def load(data)
+	  	overrides = {}
+
 	  	if data
 	    	obj = raw_load(data)
-
 	    	raise TypeError, "Deserialized object is not of type #{self.class.settings_class.name}. Got #{obj.class}" unless obj.is_a?(self.class.settings_class)
-
-	    	# The reason we do the following is because defaults may have changed
-	    	# after the point of serialization. To avoid not having them, we instantiate the class
-	    	# again, we the previous values. This prevents not having data.
-	    	obj.class.new(obj.as_hash)
+	    	overrides = obj.as_hash
 	    end
+
+    	self.settings_class.new(overrides)
 	  end
 
 	  # Called to convert from ruby object to serialized data.
 	  def dump(obj)
 	  	raise TypeError, "Serialization failed: Object is not of type #{self.class.settings_class.name}." if !obj.is_a?(self.class.settings_class) && !obj.nil?
 
-	  	# We need to use the registry because we don't want to store defaults that weren't overwritten
 	  	if obj.nil?
 	  		self.class.settings_class.new
 	    else
